cec75ad7b3751da51122bbab8bd220b797d3f5c7
--add <andrewksmoker@gmail.com>
2021-01-26 12:32:22 -0500

    commit tree fdc70dce960f743f8d774cbe35d153ce46057e59

--- index.js
+++ index.js
@@ -10,4 +10,16 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(string) {
+  const hour = string.split(':')[0];
+  const integer = parseInt(hour, 10);
+  if (integer < 12) return "Good Morning"
+  if (integer > 17) return "Good Evening"
+  return "Good Afternoon"
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string) {
+  document.getElementById("greeting").innerText = string;
+}
